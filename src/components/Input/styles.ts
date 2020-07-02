2be962b20b7fefdcdefeea16d0db396de51d1f49
lucascprazeres <olucascostadp@gmail.com>
2020-07-02 13:05:23 -0300

    minor: muda a cor do texto do input 🎨

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled.div`
   border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  color: #666360;
 
   display: flex;
   align-items: center;
@@ -19,6 +18,7 @@ export const Container = styled.div`
     background: transparent;
     border: 0;
     flex: 1;
+    color: #f4ede8;
 
     &::placeholder {
       color: #666360;
