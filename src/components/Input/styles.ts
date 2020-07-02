5516040431614f810a2b3506ebdcf87bf8080ac9
lucascprazeres <olucascostadp@gmail.com>
2020-07-02 13:59:33 -0300

    minor: inclui efeitos de foco e preenchimento no componente de Input 💅

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,11 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+  color: #666360;
 
   display: flex;
   align-items: center;
@@ -13,6 +19,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     background: transparent;
