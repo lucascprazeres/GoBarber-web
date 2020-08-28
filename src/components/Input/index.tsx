5ce48f14b946274e68db67e414c3240ffacec15a
lucascprazeres <olucascostadp@gmail.com>
2020-08-28 19:54:01 -0300

    test: cria testes do componente de input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -14,7 +14,7 @@ import { Container, Error } from './styles';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   containerStyle?: object;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -54,6 +54,7 @@ const Input: React.FC<InputProps> = ({
       isFocused={isFocused}
       isFilled={isFilled}
       isErrored={!!error}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
